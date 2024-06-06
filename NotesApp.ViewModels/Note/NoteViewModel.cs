bae816d17bec8d63f1dfc87e26f73dbb8e0ae0d2
Georgi <Georgiivanover@gmail.com>
2024-06-06 14:55:34 +0300

    Add/Edit note validation

--- NotesApp.ViewModels/Note/NoteViewModel.cs
+++ NotesApp.ViewModels/Note/NoteViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,10 @@ namespace NotesApp.ViewModels.Note
     public class NoteViewModel
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string? Content { get; set; }
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Content { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public DateTime LastUpdatedAt { get; set; }
     }
