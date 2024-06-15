1062638bafb9b29172703cc8995f8ecc952e48ab
Georgi <Georgiivanover@gmail.com>
2024-06-15 15:45:20 +0300

    Note service added try-catch blocks.

--- NotesApp.Core/NoteService.cs
+++ NotesApp.Core/NoteService.cs
@@ -15,36 +15,60 @@ namespace NotesApp.Core
         }
         public async Task AddAsync(NoteFormModel model, Guid userId)
         {
-            Note note = new Note
+            try
             {
-                Title = model.Title,
-                Content = model.Content,
-                CreatedAt = DateTime.Now,
-                CreatorId = userId,
+                Note note = new Note
+                {
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedAt = DateTime.Now,
+                    CreatorId = userId,
 
-            };
-            await this._db.AddAsync(note);
-            await this._db.SaveChangesAsync();
+                };
+                await this._db.AddAsync(note);
+                await this._db.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("An unexpected error occurred while adding new note.");
+            }
+           
         }
 
         public async Task DeleteAsync(int noteId)
         {
-            Note note = await this._db.Notes.FirstOrDefaultAsync(n => n.Id == noteId);
+            try
+            {
+                Note note = await this._db.Notes.FirstOrDefaultAsync(n => n.Id == noteId);
 
-            this._db.Notes.Remove(note);
-            await this._db.SaveChangesAsync();
+                this._db.Notes.Remove(note);
+                await this._db.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("An unexpected error occurred while deleting your note.");
+            }
+            
         }
 
         public async Task EditAsync(NoteViewModel model)
         {
-            Note noteToEdit = await this._db.Notes
+            try
+            {
+                Note noteToEdit = await this._db.Notes
                 .FirstOrDefaultAsync(n => n.Id == model.Id);
 
-            noteToEdit.Title = model.Title;
-            noteToEdit.Content = model.Content;
-            noteToEdit.LastUpdatedAt = DateTime.Now;
+                noteToEdit.Title = model.Title;
+                noteToEdit.Content = model.Content;
+                noteToEdit.LastUpdatedAt = DateTime.Now;
 
-            await this._db.SaveChangesAsync();
+                await this._db.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new ApplicationException("An unexpected error occurred while editing your note.");
+            }
+            
         }
 
         public async Task<IEnumerable<NoteViewModel>> GetAllAsync(Guid userId)
